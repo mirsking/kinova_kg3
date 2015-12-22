6e46be9cb3d0080bdc73953346fb0920519479fe
mirsking <mirsking@gmail.com>
2015-12-22 17:15:16 +0800

    add ln2gazebo to ln the resources to gazebo

--- urdf/generate_sdf.sh
+++ urdf/generate_sdf.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-sudo rm /usr/bin/qqtcreator
-sudo ln -s $BASH_DIR/../shortcuts/qtcreator /usr/bin/qqtcreator " ) )"
 rosrun xacro xacro kinova_kg3.xacro > kinova_kg3.urdf
-gz sdf -p kinova_kg3.urdf > ../gazebo/kinova_kg3.sdf
+gz sdf -p kinova_kg3.urdf > ../gazebo/models/kinova_kg3.sdf

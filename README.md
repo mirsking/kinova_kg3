b08d7f46c499735f22dd6b2e3def44153e08111e
mirsking <mirsking@gmail.com>
2015-12-22 17:26:34 +0800

    add ros launch file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # kinova_kg3
 ros and gazebo model of kinova's hand kg3
+
+## Use in Pure Gazebo
+Step 1. generate sdf
+```
+cd urdf
+./generate_sdf.sh
+```
+
+Step 2. ln gazebo models to gazebo default directory
+```
+./ln2gazebo.sh
+```
+
+## Use in ROS
+Step 1. Clone the code in a ros workspace src folder.
+```
+cd /workspace/folder/
+catkin_make
+source devel/setup.bash
+```
+
+Step 2. Launch the Kg3 world
+```
+roslaunch kinova_kg3 kinova_kg3.launch
+```
--- ln2gazebo.sh
+++ ln2gazebo.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# qtcreator
 rm ~/.gazebo/models/kinova_kg3 -rf
 ln -s $BASH_DIR/meshes $BASH_DIR/gazebo/models/meshes
 ln -s $BASH_DIR/gazebo/models ~/.gazebo/models/kinova_kg3
-#GAZEBO_MODEL_PATH=$BASH_DIR/gazebo:$GAZEBO_MODEL_PATH
-#export GAZEBO_MODEL_PATH 
-#GAZEBO_RESOURCE_PATH=$BASH_DIR/gazebo:$GAZEBO_RESOURCE_PATH
-#export GAZEBO_RESOURCE_PATH 
-#echo $GAZEBO_MODEL_PATH 
-#echo $GAZEBO_RESOURCE_PATH 
-#ln -s $BASH_DIR/gazebo/model.config ~/.gazebo/models/kinova_kg3/model.config
-#ln -s $BASH_DIR/gazebo/kionva_kg3.world ~/.gazebo/models/kinova_kg3/kinova_kg3.world

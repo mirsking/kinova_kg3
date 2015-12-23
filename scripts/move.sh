aca4dd96dbda8e4ef0a093854d9933a26df191ef
mirsking <mirsking@gmail.com>
2015-12-23 14:29:47 +0800

    change gazebo5 to gazebo2 to use gazebo_ros

--- scripts/move.sh
+++ scripts/move.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-FINGER_JOINT_VALUE=0.0
-TIP_JOINT_VALUE=10.0
+FINGER_JOINT_VALUE=20.0
+TIP_JOINT_VALUE=40.0
 
 
 rostopic pub -1 /kinova_kg3/jaco_joint_finger_1_position_controller/command std_msgs/Float64 "data: $FINGER_JOINT_VALUE"
-rostopic pub -1 /kinova_kg3/jaco_joint_finger_tip_1_position_controller/command std_msgs/Float64 "data: $TIP_JOINT_VALUE"
-
 rostopic pub -1 /kinova_kg3/jaco_joint_finger_2_position_controller/command std_msgs/Float64 "data: $FINGER_JOINT_VALUE"
-rostopic pub -1 /kinova_kg3/jaco_joint_finger_tip_2_position_controller/command std_msgs/Float64 "data: $TIP_JOINT_VALUE"
-
 rostopic pub -1 /kinova_kg3/jaco_joint_finger_3_position_controller/command std_msgs/Float64 "data: $FINGER_JOINT_VALUE"
+
+rostopic pub -1 /kinova_kg3/jaco_joint_finger_tip_1_position_controller/command std_msgs/Float64 "data: $TIP_JOINT_VALUE"
+rostopic pub -1 /kinova_kg3/jaco_joint_finger_tip_2_position_controller/command std_msgs/Float64 "data: $TIP_JOINT_VALUE"
 rostopic pub -1 /kinova_kg3/jaco_joint_finger_tip_3_position_controller/command std_msgs/Float64 "data: $TIP_JOINT_VALUE"
--- urdf/generate_sdf.sh
+++ urdf/generate_sdf.sh
@@ -1,2 +1,2 @@
 rosrun xacro xacro kinova_kg3.xacro > kinova_kg3.urdf
-gz sdf -p kinova_kg3.urdf > ../gazebo/models/kinova_kg3.sdf
+gzsdf print kinova_kg3.urdf > ../gazebo/models/kinova_kg3.sdf
